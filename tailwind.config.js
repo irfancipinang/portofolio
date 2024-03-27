e72eb03cb1d3ca60c5f84efdb63ee16e4da1093a
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-03-27 09:48:18 +0700

    Portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
