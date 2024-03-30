420f24038cea2fc4dc704220a8e750ef8b710b88
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-03-31 05:50:06 +0700

    portofolio

---
+++ type.js
@@ -0,0 +1,29 @@
+const words = ["Hello","Nama Saya", "Muhammad Irfan Maulana"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 150);
+}
+
+type();
